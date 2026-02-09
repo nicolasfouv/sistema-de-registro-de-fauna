df0e7ee4007b5045d67e404934c5e6eaed9b6e8a
nicolas-main <nicolas.fouverney@gmail.com>
2026-02-09 15:19:09 -0300

    feat: add audit log feature on database (prisma)

--- srf-backend/src/models/formModel.ts
+++
@@ -1,7 +0,0 @@
-import z from "zod";
-import { accessLevel } from "../../generated/prisma";
-
-export const formRequest = z.object({
-    formName: z.string().nonempty({error: 'Nome do formulário inválido'}),
-    requiredLevel: z.enum(accessLevel, {error: 'Nível de acesso inválido'}).optional(),
-});
